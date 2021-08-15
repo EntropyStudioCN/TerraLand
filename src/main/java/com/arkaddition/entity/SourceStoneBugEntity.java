887c54a42edccd6796ea0d64e1eed077b71ffbe4
CanYingisme-Git <3192799549@qq.com>
2021-08-15 23:46:50 +0800

    Stupid entity AI tasks

--- src/main/java/com/arkaddition/entity/SourceStoneBugEntity.java
+++ src/main/java/com/arkaddition/entity/SourceStoneBugEntity.java
@@ -17,7 +17,7 @@ import software.bernie.geckolib3.core.event.predicate.AnimationEvent;
 import software.bernie.geckolib3.core.manager.AnimationData;
 import software.bernie.geckolib3.core.manager.AnimationFactory;
 
-public class SourceStoneBugEntity extends EntityMob implements IAnimatable {
+public class SourceStoneBugEntity extends EntityCreature implements IAnimatable {
     AnimationFactory factory = new AnimationFactory(this);
     private int size = 1;
     public SourceStoneBugEntity(World worldIn)
@@ -25,29 +25,29 @@ public class SourceStoneBugEntity extends EntityMob implements IAnimatable {
         super(worldIn);
         this.ignoreFrustumCheck = true;
         this.setSize(0.8f*size,0.4f*size);
-
     }
     protected void initEntityAI() {
         this.tasks.addTask(0, new EntityAISwimming(this));
+        //this.tasks.addTask(2, new EntityAIZombieAttack(this, 1.0D, false));
         this.tasks.addTask(5, new EntityAIMoveTowardsRestriction(this, 1.0D));
         this.tasks.addTask(7, new EntityAIWanderAvoidWater(this, 1.0D));
         this.tasks.addTask(8, new EntityAIWatchClosest(this, EntityPlayer.class, 8.0F));
         this.tasks.addTask(8, new EntityAILookIdle(this));
-        this.targetTasks.addTask(2,new EntityAIHurtByTarget(this,false, new Class[0]));
-        this.targetTasks.addTask(3,new EntityAINearestAttackableTarget<EntityPlayer>(this, EntityPlayer.class, true));
         this.applyEntityAI();
     }
 
     protected void applyEntityAI() {
-
+        this.tasks.addTask(6, new EntityAIMoveThroughVillage(this, 1.0D, false));
+        this.targetTasks.addTask(1, new EntityAIHurtByTarget(this, true, new Class[]{}));
+        this.targetTasks.addTask(2, new EntityAINearestAttackableTarget(this, EntityPlayer.class, true));
     }
     @Override
     protected void applyEntityAttributes()
     {
         super.applyEntityAttributes();
 
-        this.getEntityAttribute(SharedMonsterAttributes.MAX_HEALTH).setBaseValue(33.0D);
-        this.getEntityAttribute(SharedMonsterAttributes.MOVEMENT_SPEED).setBaseValue(0.25D);
+        //this.getEntityAttribute(SharedMonsterAttributes.MAX_HEALTH).setBaseValue(33.0D);
+        //this.getEntityAttribute(SharedMonsterAttributes.MOVEMENT_SPEED).setBaseValue(0.25D);
         //this.getEntityAttribute(SharedMonsterAttributes.ATTACK_DAMAGE).setBaseValue(1.0D);
     }
     public int getSize() {

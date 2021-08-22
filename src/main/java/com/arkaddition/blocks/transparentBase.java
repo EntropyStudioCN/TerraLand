4c037fa493a567c2da8e05a83ff44a55f771b07c
IAX <retailer673@163.com>
2021-08-22 12:26:58 +0800

    repair the BUG &ADD base                   —IAX...

---
+++ src/main/java/com/arkaddition/blocks/transparentBase.java
@@ -0,0 +1,40 @@
+package com.arkaddition.blocks;
+
+import com.arkaddition.Main;
+import com.arkaddition.creativetab.TabArkAddition3Block0;
+import com.arkaddition.init.ModBlocks;
+import com.arkaddition.init.ModItems;
+import com.arkaddition.util.IHasModel;
+import net.minecraft.block.Block;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.material.Material;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemBlock;
+import net.minecraft.util.BlockRenderLayer;
+
+public class transparentBase extends Block implements IHasModel {
+    public transparentBase(String name, Material material, SoundType sound, float hard, String Tool, int Level)
+    {
+        super(material);
+
+        setTranslationKey(name);
+        setRegistryName(name);
+        setCreativeTab(TabArkAddition3Block0.TABARKADDITION3BLOCK0);
+        setLightOpacity(1);
+        setSoundType(sound);
+        setHardness(hard);
+        setHarvestLevel (Tool,Level);
+
+
+
+        ModBlocks.BLOCKS.add(this);
+        ModItems.ITEMS.add(new ItemBlock (this).setRegistryName(this.getRegistryName()));
+    }
+
+    @Override
+    public void registerModels() {
+        Main.proxy.registerItemRenderer(Item.getItemFromBlock(this), 0, "inventory");
+    }
+
+}

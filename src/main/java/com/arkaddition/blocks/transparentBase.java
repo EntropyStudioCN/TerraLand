21e558db31ce251320752ce2170c8cc988aaeeb5
IAX <retailer673@163.com>
2021-08-22 18:48:34 +0800

    repair the BUG  again！！！！！！！！！！！！！！                   —IAX...

--- src/main/java/com/arkaddition/blocks/transparentBase.java
+++ src/main/java/com/arkaddition/blocks/transparentBase.java
@@ -6,11 +6,9 @@ import com.arkaddition.init.ModBlocks;
 import com.arkaddition.init.ModItems;
 import com.arkaddition.util.IHasModel;
 import net.minecraft.block.Block;
-import net.minecraft.block.BlockGlass;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
-import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
@@ -23,6 +21,9 @@ import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class transparentBase extends Block implements IHasModel {
+
+
+
     public transparentBase(String name, Material material, SoundType sound, float hard, String Tool, int Level)
     {
         super(material);
@@ -36,10 +37,12 @@ public class transparentBase extends Block implements IHasModel {
         setHarvestLevel (Tool,Level);
 
 
-
         ModBlocks.BLOCKS.add(this);
         ModItems.ITEMS.add(new ItemBlock (this).setRegistryName(this.getRegistryName()));
     }
+
+
+
 
     @SideOnly(Side.CLIENT)
     public BlockRenderLayer getRenderLayer() {
